ba6d9c6191825176ad8ffb1b24ac85527ddfff67
Nathan Bouquet <nbouquet04@gmail.com>
2024-11-19 13:19:11 +0100

    removed server.py class

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Register blueprints
 app.register_blueprint(customer_routes.bp)
-app.register_blueprint(dish_routes.bp)
+# app.register_blueprint(dish_routes.bp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
